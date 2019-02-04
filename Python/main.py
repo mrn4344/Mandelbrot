27dd18b5306d13b5d6462ee12ee8fa8f74ec2378
Michael Nichols <mrn4344@rit.edu>
2019-02-04 13:27:02 -0500

    First broken version.\n Points calculated, tries to display, but not succsessful.

--- Python/main.py
+++ Python/main.py
@@ -1,7 +1,11 @@
 import turtle as t
+import math
 
 width = 1600
 height = 900
+
+scale = 2
+
 
 def init():
     t.setup(width, height, 1, 1)
@@ -11,36 +15,60 @@ def init():
     t.tracer(0,1)
     t.hideturtle()
     t.pensize(1)
+    t.seth(90)
 
 def drawPixel(pos, color):
-    t.up()
+    #t.up()
     t.goto(pos)
     t.pencolor(color)
-    t.pd()
+    #t.pd()
+    #t.fd(1)
     t.goto(pos[0]+1, pos[1])
-    t.up()
+
+def magnitude(c):
+    return math.sqrt(c[0]*c[0]+c[1]*c[1])
 
 def function(z, c):
     return ( ((z[0]*z[0]) - (z[1]*z[1])) + c[0] , (2*z[0]*z[1]) + c[1])
 
 def inMandelbrot(c):
     number = (0,0)
-    for i in range(0, 1024):
+    for i in range(0, 128):
         number = function(number, c)
-        if Math.sqrt(number[0]*number[0] + number[1]*number[1]) > 2:
+        if magnitude(number) > 2:
             return False
     return True
 
-def pixelToCoord(pos):
-    return
-
+def pixelToCoord( pos ):
+    (x, y) = pos
+    return ( (1/(225*scale))*(x - width/2) , (1/(scale*225))*(height/2 - y) )
 
 
 def main():
     init()
-    drawPixel((0,0), "red")
-    drawPixel((1,0), "green")
-    drawPixel((2,0), "blue")
+    values = [([None] * height)] * width
+    for y in range(0, height):
+        for x in range(0,width):
+            c = pixelToCoord((x,y))
+            values[x][y] = inMandelbrot(c)
+        if(y%10 == 0):
+            print("Row " + str(y))
+    print("Done calc")
+    for y in range(0, height):
+        for x in range(0,width):
+            t.pd()
+            if values[x][y]:
+                drawPixel( (x,y), (0,0,0))
+            else:
+                drawPixel( (x,y), '#ff0000')
+            t.pu()
+            #print("Trying point" + str(c))
+            #if inMandelbrot(c):
+            #    drawPixel( (x,y), (0,0,0))
+            #else:
+            #    drawPixel( (x,y), '#ff0000')
+
+        t.update()
     # t.goto(0,0)
     # t.pd()
     # t.goto(1600,0)
@@ -49,7 +77,7 @@ def main():
     # t.goto(0,1)
     # t.pd()
     # t.goto(1600,1)
-    t.update()
+    #t.update()
     t.exitonclick()
 
 if __name__ == "__main__":
