2fbcc26bcb650db5d2f8be8667ddbd089cbd0edd
Michael Nichols <mrn4344@rit.edu>
2019-02-11 10:18:59 -0500

    Fixed scaling functions and changed picture resolution

--- Python/main.py
+++ Python/main.py
@@ -1,26 +1,27 @@
 import mandelbrot as mand
 from PIL import Image
 
-width = 1600
-height = 900
+width = 1280
+height = 720
 
 scale = 2
 
 def pixelToCoord( pos ):
     (x, y) = pos
-    return ( (1/(225*scale))*(x - width/2) , (1/(scale*225))*(height/2 - y) )
+    return ( 4*(x/height - 0.5)/scale ,  -4*(y/height - 0.5)/scale)
 
 
 def main():
     me = mand.mandelbrot(2)
-    img = Image.new('RGB', (1920,1080), color = 'white')
+    img = Image.new('RGB', (width,height), color = 'white')
     for y in range(0, height):
         for x in range(0,width):
             c = pixelToCoord((x,y))
             if(me.isInSet(complex(c[0], c[1]), 1024)):
                 img.putpixel((x,y), (0,0,0))
-        print("Row " + str(y))
-    img.save("output1.png")
+        if y%25 == 0:
+            print("Row " + str(y))
+    img.save("output2.png")
 
 if __name__ == "__main__":
     main()
